076d21cad90039bbe2dbb108ce30e1de84cb92e9
Serin Yoon <syoon17@illinois.edu>
2015-07-31 01:28:42 -0500

    fix node issue

--- mean-setup.sh
+++ mean-setup.sh
@@ -1,9 +1,10 @@
 # Install node
 sudo apt-get update
+sudo apt-get install -y git
 sudo apt-get install nodejs
-sudo ln -s `which nodejs` /usr/bin/node
-sudo chown vagrant /usr/bin/node
-
+sudo apt-get install nodejs-legacy
+sudo apt-get install npm
+sudo chown -R `whoami` /usr/local
 
 # install global node packages
 npm install -g nodemon
@@ -13,3 +14,4 @@ npm install -g grunt-cli
 # install ruby and sass
 sudo apt-get install ruby-full
 sudo su -c "gem install sass"
+sudo su -c "gem install compass"
